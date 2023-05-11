2b65d874e7a3da004564abffd5e6c72a6e244333
Aytuhan Kazandirmak <aytuhann@gmail.com>
2023-05-11 20:44:48 +0300

    BEM ile düzenleme

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
-$(window).scroll(function(){
-    if($(window).scrollTop() > 650){
-        $(".grid").css({
-            "max-height": "60px",
-            "padding": "0px",
-            "box-shadow": "0px 0px 5px 0px",
-        })
-    }else{
-        $(".grid").css({
-            "max-height": "100px",
-            "padding": "20px",
-            "box-shadow": "none",
-        })
-}
-})
-        $(document).ready(function () {
-            $(".li-element a").click(function () {
-                $(".nav a").css({ "border-bottom": "none" })
-                $(this).css({
-                    "border-bottom": "2px solid black",
-
-                })
-            })
-        })
-        $(window).scroll(function () {
-            if ($(window).scrollTop() == 0) {
-                $(".grid").css({
-                    "box-shadow": "none"
-                })
-            } else {
-                $(".grid").css({
-                    "box-shadow": "0px 0px 5px 0px"
-                })
-            }
-        })
-
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 650) {
+    $(".navigation").css({
+      "max-height": "60px",
+      padding: "0px",
+      "box-shadow": "0px 0px 5px 0px",
+    });
+  } else {
+    $(".navigation").css({
+      "max-height": "100px",
+      padding: "20px",
+      "box-shadow": "none",
+    });
+  }
+});
+$(document).ready(function () {
+  $(".navigation__navbar_link__element__link-button").click(function () {
+    $(".navigation__navbar_link__element__link-button").css({
+      "border-bottom": "none",
+    });
+    $(this).css({
+      "border-bottom": "2px solid black",
+    });
+  });
+});
+$(window).scroll(function () {
+  if ($(window).scrollTop() == 0) {
+    $(".navigation").css({
+      "box-shadow": "none",
+    });
+  } else {
+    $(".navigation").css({
+      "box-shadow": "0px 0px 5px 0px",
+    });
+  }
+});
