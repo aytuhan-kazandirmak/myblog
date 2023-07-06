86f8c315b60de40f9d34be5d2a43093816ce1e43
Aytuhan Kazandirmak <aytuhann@gmail.com>
2023-07-06 14:38:44 +0300

    change

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ $(window).scroll(function () {
   if ($(window).scrollTop() > 550) {
     $(".navigation").css({
       "max-height": "60px",
-      padding: "20px",
+      padding: "15px",
       "box-shadow": "0px 0px 5px 0px",
     });
   } else {
     $(".navigation").css({
-      "max-height": "100px",
-      padding: "20px",
+      "max-height": "110px",
+      padding: "15px",
       "box-shadow": "none",
     });
   }
